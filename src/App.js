04d7d49016cc0bc7afa381b670bd22d7bb2ab4ae
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-04-17 15:36:43 +0200

    finished example project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { Products } from 'components/Products'
 import { Cart } from 'components/Cart'
+import { cart } from 'reducers/cart';
+import { products } from 'reducers/products';
 
-export const App = () => (
-  <>
-    <Cart />
-    <Products />
-  </>
-)
+/* Standard redux-way to set up state by combining reducers and then configuring store
+based on combination */
+const reducer = combineReducers({
+  cart: cart.reducer,
+  products: products.reducer
+})
+
+const store = configureStore({ reducer })
+
+export const App = () => {
+  return (
+    <Provider store={store}>
+      <Cart />
+      <Products />
+    </Provider>
+  )
+};
+
